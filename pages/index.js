c6aee3fdc503f3fb1b3d47943d6465b07bb1cc98
nateos <sodjanathan@gmail.com>
2021-10-12 15:38:59 +0000

    major static deign done

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import displayPic from "../public/assets/crello-255294772-stock-photo-beautiful-african-american-woman-body.jpeg";
+import profilePicture from "../public/assets/profile.jpg";
 
 export default function Home() {
   return (
@@ -36,18 +37,28 @@ export default function Home() {
               className="preview-image"></Image>
           </div>
         </aside>
-        <section>
-          <div>
-            <h1>Host Lequite Artisitc</h1>
-            <div>
-              <p>Xels Trends</p>
+        <section className="item-details container">
+          <div className="item-title">
+            <h1 className="title">Host Lequite Artisitc</h1>
+            <div className="profile-info">
+              <small className="profile-name">Xels Trends</small>
               {/* profile image here */}
+              <span className="profile-image-container">
+                <Image
+                  src={profilePicture}
+                  alt="profile picture"
+                  className="profile-image"
+                  width={30}
+                  height={30}></Image>
+              </span>
             </div>
           </div>
-          <div>
-            <p>Formal Wears</p>
-            <p>Available</p>
-            <p>100% cotton</p>
+          <div className="">
+            <ul className="item-desc">
+              <li className="list-item">Formal Wears</li>
+              <li className="list-item">Available</li>
+              <li className="list-item">100% cotton</li>
+            </ul>
           </div>
           <div>
             <p>Select Color</p>
@@ -69,11 +80,11 @@ export default function Home() {
             expedita aliquam doloremque praesentium, vitae voluptatum quaerat
             consequatur optio facilis adipisci harum quisquam minus.
           </p>
-          <div>
-            <ul>
-              <li>Option 1</li>
-              <li>Option 2</li>
-              <li>Option 3</li>
+          <div className="options">
+            <ul className="options__list">
+              <li className="list-item">Option 1</li>
+              <li className="list-item">Option 2</li>
+              <li className="list-item">Option 3</li>
             </ul>
           </div>
         </section>
